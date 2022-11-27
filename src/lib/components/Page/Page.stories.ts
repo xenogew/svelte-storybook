854a139595c2999b7cd18ad1156d1ebdf499f5c5
Natta Wang <xenogew@gmail.com>
2022-11-27 20:47:52 +0900

    formatted

--- src/lib/components/Page/Page.stories.ts
+++ src/lib/components/Page/Page.stories.ts
@@ -2,26 +2,26 @@ import { within, userEvent } from '@storybook/testing-library';
 import Page from '../Page/Page.svelte';
 
 export default {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/svelte/configure/story-layout
-    layout: 'fullscreen',
-  },
+	title: 'Example/Page',
+	component: Page,
+	parameters: {
+		// More on Story layout: https://storybook.js.org/docs/svelte/configure/story-layout
+		layout: 'fullscreen'
+	}
 };
 
-const Template = (args:any) => ({
-  Component: Page,
-  props: args,
+const Template = (args: any) => ({
+	Component: Page,
+	props: args
 });
 
-export const LoggedOut:any = Template.bind({});
+export const LoggedOut: any = Template.bind({});
 LoggedOut.args = {};
 
 // More on interaction testing: https://storybook.js.org/docs/svelte/writing-tests/interaction-testing
-export const LoggedIn:any = Template.bind({});
-LoggedIn.play = async ({ canvasElement }:any) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+export const LoggedIn: any = Template.bind({});
+LoggedIn.play = async ({ canvasElement }: any) => {
+	const canvas = within(canvasElement);
+	const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+	await userEvent.click(loginButton);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
+	}
 };
 
 export default defineConfig(config);
